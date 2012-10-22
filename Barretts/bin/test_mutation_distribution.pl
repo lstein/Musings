28ff42eeee243a242cb31b66039d6050748d9870
Lincoln Stein <lincoln.stein@gmail.com>
2012-10-22 07:20:03 -0400

    needs work

--- Barretts/bin/test_mutation_distribution.pl
+++ Barretts/bin/test_mutation_distribution.pl
@@ -6,7 +6,8 @@ use lib "$Bin/../lib";
 use Simulate::MutationDistribution;
 
 my $md = Simulate::MutationDistribution->new;
-$md->generate(100,100_000,5);
+$md->generate_power_distribution();
+$md->generate_mutations();
 $md->print_distribution;
 
 exit 0;
--- Barretts/lib/Simulate/MutationDistribution.pm
+++ Barretts/lib/Simulate/MutationDistribution.pm
@@ -4,39 +4,60 @@ use strict;
 use warnings;
 use Carp 'croak';
 
+use constant euler => 2.71828;
+
 sub new {
     my $class = shift;
     return bless {
 	mutations       => [],
 	frequencies     => {},
+	spectrum        => [],
     },ref $class || $class;
 }
 
 sub mutations   {shift->{mutations}   }
 sub frequencies {shift->{frequencies} }
+sub spectrum    {shift->{spectrum}    }
 
-sub generate {
+# from http://www.alexeypetrov.narod.ru/Eng/progs.html#r_gener
+sub generate_power_distribution {
     my $self = shift;
-    my ($mutation_count,$iterations,$expansion) = @_;
-    $iterations ||= 100_000;
-    $expansion  ||= 5;
+    my ($N,$alpha,$x_min,$x_max) = @_;
+    
+    $N             ||= 300000;
+    $alpha         ||= -2;
+    $x_min         ||= 0.05;
+    $x_max         ||= 0.8;
+
+    my $p0 = $x_min ** ($alpha+1);
+    my $p1 = $x_max ** ($alpha+1);
+    my $s  = $self->spectrum;
+    for (my $i=0; $i<$N; $i++) {
+	my $r = rand() or next;
+	my $E = ($r*($p1-$p0)+$p0)**(1/($alpha+1));
+	push @$s,$E;
+    }
+}
+
+sub generate_mutations {
+    my $self  = shift;
+    my ($count,$dist_size) = @_;
+    $count     ||= 100;
+    $dist_size ||= 100_000;
+    my $spectrum  = $self->spectrum;
     my $mutations = $self->mutations;
-
-    # initial set
-    @$mutations = (1..$mutation_count);
-
-    # random duplications
-    for (1..$iterations) {
-	my $count = @$mutations;
-	my $lucky = $mutations->[rand $count];
-	for (1..$expansion) {
-	    push @$mutations,$lucky;
-	}
+    my $freq      = $self->frequencies;
+    for (1..$count) {
+	my $label = sprintf("M%03d",$_);
+	my $freq  = $spectrum->[rand @$spectrum];
+	warn $freq,"\n";
+	my $c     = int($count * $freq);
+	my @dist  = ($label) x $c;
+	push @$mutations,@dist;
     }
 
-    my $frequencies = $self->frequencies;
-    foreach (@$mutations) {
-	$frequencies->{$_}++;
+    for my $m (@$mutations) {
+	$freq->{$m}++;
     }
 }
 
@@ -49,7 +70,7 @@ sub print_distribution {
 	$total += $_;
     }
     for my $mut (sort {$f->{$b}<=>$f->{$a}} keys %$f) {
-	printf ("%5d %5.4f\n",$mut,$f->{$mut}/$total);
+	printf ("%5s %5.4f\n",$mut,$f->{$mut}/$total);
     }
 }
 
