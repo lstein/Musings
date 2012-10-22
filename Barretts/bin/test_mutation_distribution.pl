2bb99340082f26cce8a4229f817a48adf529a66f
Lincoln Stein <lincoln.stein@gmail.com>
2012-10-22 15:09:53 -0400

    clone generation working

--- Barretts/bin/test_mutation_distribution.pl
+++ Barretts/bin/test_mutation_distribution.pl
@@ -6,9 +6,25 @@ use lib "$Bin/../lib";
 use Simulate::MutationDistribution;
 
 my $md = Simulate::MutationDistribution->new;
-$md->generate_power_distribution();
-#$md->generate_mutations();
-#$md->print_distribution;
+$md->generate_mutations();
+$md->print_distribution;
+
+print ">>>>>>>>>>>>>>>\n";
+
+my @clones = map {[$md->generate_clone]} (1..100);
+
+my (%freq,$total);
+for my $c (@clones) {
+    my @mutations = @$c;
+    $freq{$_}++ foreach @mutations;
+    $total   += @mutations;
+}
+
+foreach (sort {$freq{$b} <=> $freq{$a}} keys %freq) {
+    $total += $freq{$_};
+    printf("%5s %5.4f\n",$_,$freq{$_}/100);
+}
+print $total,"\n";
 
 exit 0;
 
--- Barretts/lib/Simulate/MutationDistribution.pm
+++ Barretts/lib/Simulate/MutationDistribution.pm
@@ -11,77 +11,48 @@ sub new {
     return bless {
 	mutations       => [],
 	frequencies     => {},
-	spectrum        => [],
     },ref $class || $class;
 }
 
 sub mutations   {shift->{mutations}   }
 sub frequencies {shift->{frequencies} }
-sub spectrum    {shift->{spectrum}    }
 
 # from http://www.alexeypetrov.narod.ru/Eng/progs.html#r_gener
-sub generate_power_distribution {
+sub generate_mutations {
     my $self = shift;
     my ($N,$alpha,$x_min,$x_max) = @_;
     
-    $N             ||= 300000;
+    $N             ||= 100;
     $alpha         ||= -2;
     $x_min         ||= 0.05;
-    $x_max         ||= 0.8;
+    $x_max         ||= 1.0;
 
     my $p0 = $x_min ** ($alpha+1);
     my $p1 = $x_max ** ($alpha+1);
-    my $s  = $self->spectrum;
     for (my $i=0; $i<$N; $i++) {
 	my $r = rand() or next;
-	my $E = ($r*($p1-$p0)+$p0)**(1/($alpha+1));
-	print $E,"\n";;
-	push @$s,$E;
-    }
-}
-
-sub generate_mutations {
-    my $self  = shift;
-    my ($count,$dist_size) = @_;
-    $count     ||= 100;
-    $dist_size ||= 100_000;
-    my $spectrum  = $self->spectrum;
-    my $mutations = $self->mutations;
-    my $freq      = $self->frequencies;
-    for (1..$count) {
-	my $label = sprintf("M%03d",$_);
-	my $freq  = $spectrum->[rand @$spectrum];
-	warn $freq,"\n";
-	my $c     = int($count * $freq);
-	my @dist  = ($label) x $c;
-	push @$mutations,@dist;
-    }
-
-    for my $m (@$mutations) {
-	$freq->{$m}++;
+	my $E = ($r*($p1-$p0)+$p0)**(1/($alpha+1)); # frequency of this one
+	my $label = sprintf("M%03d",$i+1);
+	$self->frequencies->{$label}=$E;
     }
 }
 
 sub print_distribution {
     my $self = shift;
     my $f     = $self->frequencies;
-    my $total = 0;
-
-    foreach (values %$f) {
-	$total += $_;
-    }
     for my $mut (sort {$f->{$b}<=>$f->{$a}} keys %$f) {
-	printf ("%5s %5.4f\n",$mut,$f->{$mut}/$total);
+	printf ("%5s %5.4f\n",$mut,$f->{$mut});
     }
 }
 
-sub pick_mutations {
+sub generate_clone {
     my $self = shift;
     my $count = shift || 1;
-    my $m     = $self->mutations;
+    my $f     = $self->frequencies;
+    my @keys  = keys %$f;
     my @result;
-    for (1..$count) {
-	push @result,$m->[rand @$m];
+    for my $c (@keys) {
+	push @result,$c if rand() <= $f->{$c};
     }
     return @result;
 }
