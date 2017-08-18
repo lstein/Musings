19d08b2a9a7121ecca8442087b9b277dbad21094
Lincoln Stein <lincoln.stein@gmail.com>
2017-08-18 16:03:14 -0400

    working

--- PCAWG-13-Subtypes/revise_diagnosis.pl
+++ PCAWG-13-Subtypes/revise_diagnosis.pl
@@ -8,11 +8,12 @@ my $revisions = shift;
 
 my %diagnosis;
 open my $f,$revisions or die "$revisions: $!";
-while (<>) {
+while (<$f>) {
     next if /^#/;
-    my ($specimen_id,$original_icd,$new_icd,$comment) = split /\s+/;
-    my ($histological_type)    = $comment=~/->(.+)/;
-    $diagnosis{$specimen_id} = [$new_icd,$histological_type];
+    my ($specimen_id,$original_icd,$new_icd,@rest) = split /\s+/;
+    my $comment             = join " ",@rest;
+    my ($histological_type) = $comment=~/->(.+)/;
+    $diagnosis{$specimen_id}= [$new_icd,$histological_type];
 }
 close $f;
 
@@ -20,13 +21,13 @@ close $f;
 chomp (my $first_line = <>);
 
 $first_line =~ s/^#\s+//;
-my @fields = split /\s+/,$first_line;
+my @fields = split "\t",$first_line;
 
 print "# $first_line\n";
 my %fields;
 while (<>) {
     chomp;
-    @fields{@fields} = split /\s+/;
+    @fields{@fields} = split "\t";
     my $dx = $diagnosis{$fields{'icgc_specimen_id'}} or next;
     $fields{'tumour_histological_code'} = $dx->[0];
     $fields{'tumour_histological_type'} = $dx->[1];
